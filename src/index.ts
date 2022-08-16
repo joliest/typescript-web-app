ee1ed470fcd9bbd3740a6d2cd0f77b0a74348ba9
Joliver Estampador <joliverestampador@gmail.com>
2022-08-17 07:34:17 +0800

    created User class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('hello there');
+import { User } from './models/User';
+
+const user = new User({ name: 'Joli', age: 30 });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
