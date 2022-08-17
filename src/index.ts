6d92a15629493455609c56a2b5f21f712b9f4686
Joliver Estampador <joliverestampador@gmail.com>
2022-08-17 08:02:39 +0800

    storing event listeners

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { User } from './models/User';
 
 const user = new User({});
 
-user.set({ age: 60 });
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on('change', () => {});
+
+console.log(user);
