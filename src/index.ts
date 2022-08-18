f75454afb873c3474787e63352cf40aacb87f28e
Joliver Estampador <joliverestampador@gmail.com>
2022-08-18 08:24:34 +0800

    creating Eventing class
    - hardcoded Eventing on User class for now

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,9 @@ const user = new User({ id: 1 });
 
 user.set({ name: 'dudu', age: 10 });
 user.save();
+
+user.events.on('change', () => {
+  console.log('change!');
+});
+
+user.events.triggger('change');
