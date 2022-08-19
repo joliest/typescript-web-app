6143257d610934876f084fe3bb94d9cd2bbddd61
Joliver Estampador <joliverestampador@gmail.com>
2022-08-19 08:58:40 +0800

    new generic syntax
    https://docs.google.com/document/d/1mlasXzzYuh95Wlj7x-JRJwKNbWy8xH-OO6NJqYVjj30/edit#heading=h.bt1yoyfe3jew

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,11 +1,32 @@
+import { UserProps } from './User';
+
 export class Attributes<T> {
   constructor(private data: T) {}
 
-  get(propName: string): number | string {
-    return this.data[propName];
+  /**
+   * K is a convention only
+   * <K extends keyof T> - sets a generic constraints. Limits the type 'K' can be
+   *    -> 'keyof' one of the keys of 'T'
+   * (key: K) 0 whatever arg we passed in, its a type 'K'. it can be name, age or id if we user UserProps
+   * T[K] -> normal object look up
+   *    -> T is UserProp object
+   *    -> K is either name, age or id (propeties of UserProp interface)
+   */
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
   }
 
   set(update: T): void {
     Object.assign(this.data, update);
   }
 }
+
+const attrs = new Attributes<UserProps>({
+  id: 5,
+  age: 20,
+  name: 'whatever',
+});
+
+const name = attrs.get('name');
+const age = attrs.get('age');
+const id = attrs.get('id');
