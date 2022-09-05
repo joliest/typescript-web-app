de9ca87edce108f937201ef913fc90b865ee6f53
Joliver Estampador <joliverestampador@gmail.com>
2022-09-06 07:50:31 +0800

    saving the form

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -7,6 +7,7 @@ export class UserForm extends View<User, UserProps> {
     return {
       'click:.set-age': this.onSetAge,
       'click:.set-name': this.onSetName,
+      'click:.save-model': this.onSaveClick,
     };
   }
 
@@ -25,15 +26,17 @@ export class UserForm extends View<User, UserProps> {
     }
   };
 
+  onSaveClick = (): void => {
+    this.model.save();
+  };
+
   template(): string {
     return `
       <div>
-        <h1>User Form</h1>
-        <div>User name: ${this.model.get('name')}</div>
-        <div>User name: ${this.model.get('age')}</div>
-        <input />
+        <input placeholder="${this.model.get('name')}" />
         <button class="set-name">Change Name</button>
         <button class="set-age">Set Random Age</button>
+        <button class="save-model">Save User</button>
       </div>
     `;
   }
